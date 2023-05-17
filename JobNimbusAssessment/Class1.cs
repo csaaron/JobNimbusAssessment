cf3ba81c4393e9ec1510b0dc11619997c8cf57ac
Admin <Admin@DESKTOP-NIJCJP6>
2023-05-16 23:32:00 -0600

    Add project files.

---
+++ JobNimbusAssessment/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace JobNimbusAssessment
+{
+    public class Class1
+    {
+        public static bool HasMatchingBrackets(string target)
+        {
+            const char OPEN_BRACKET = '<';
+            const char CLOSE_BRACKET = '>';
+            
+            if (target == null)
+            {
+                return true;
+            }
+
+            Stack<char> openBrackets = new Stack<char>();
+            for (int i = 0; i < target.Length; i++)
+            {
+                if (target[i] == OPEN_BRACKET)
+                {
+                    openBrackets.Push(OPEN_BRACKET);
+                }
+
+                if (target[i] == CLOSE_BRACKET && openBrackets.Count < 1)
+                {
+                    return false;
+                }
+                else if (target[i] == CLOSE_BRACKET)
+                {
+                    openBrackets.Pop();
+                }
+            }
+
+            return openBrackets.Count == 0;
+        }
+    }
+}
