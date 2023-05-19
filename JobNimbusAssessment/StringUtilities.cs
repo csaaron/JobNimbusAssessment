b127cdef7b3439f48ffb6e1af9af67adc9c41ebb
Aaron B <ambellis@outlook.com>
2023-05-18 23:52:37 -0600

    Add comment to HasMatchingBrackets method

--- JobNimbusAssessment/StringUtilities.cs
+++ JobNimbusAssessment/StringUtilities.cs
@@ -2,6 +2,11 @@
 {
     public static class StringUtilities
     {
+        /// <summary>
+        /// Takes a string, target, and returns whether or not characters in the string have matching brackets. 
+        /// Meaning for every '<' there is a corresponding '>'
+        /// </summary>
+        /// <returns>True if the string contains matching brackets, false if not.</returns>
         public static bool HasMatchingBrackets(string target)
         {
             const char OPEN_BRACKET = '<';
